178c67e4cfd8634bd9016544aabb5e6857ea5f89
chita-catalin <chita.catalin.adrian@gmail.com>
2022-03-01 23:22:40 +0200

    test

--- src/routing/index.tsx
+++ src/routing/index.tsx
@@ -6,15 +6,9 @@ export const AppRouting = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route
-          path={process.env.PUBLIC_URL + "/my-place/"}
-          element={<>home</>}
-        ></Route>
-        <Route
-          path={process.env.PUBLIC_URL + "/my-place/IDE"}
-          element={<IDE />}
-        ></Route>
-        <Route path="*" element={<>404 test</>}></Route>
+        <Route path="/my-place/" element={<>home</>}></Route>
+        <Route path="/my-place/IDE" element={<IDE />}></Route>
+        <Route path="*" element={<>404 test please work</>}></Route>
       </Routes>
     </BrowserRouter>
   );
